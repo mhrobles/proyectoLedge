a411f3b96995b0d03a81f3dc327b5ac119507653
mhrobles <mhrobles@uc.cl>
2023-12-02 23:12:13 -0300

    add: primera version de backend con bd hardcodeada

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const Koa = require('koa');
+const { default: koaBody } = require('koa-body');
+const KoaLogger = require('koa-logger');
+const cors = require('@koa/cors');
+
+const router = require('./routes');
+
+const app = new Koa();
+
+app.use(cors())
+
+app.use(KoaLogger());
+app.use(koaBody());
+
+app.use(router.routes());
+
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000');
+    });
+
+module.exports = app;
