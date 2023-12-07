0178c5d1abf55c004cf65c0c70567b9d3446b09a
mhrobles <mhrobles@uc.cl>
2023-12-07 14:42:21 -0300

    fix: error de uso de dotenv

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,8 @@
 /* eslint no-console: "off" */
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const port = process.env.PORT_BACKEND || 3002;
 
 const api = axios.create({
-    baseURL: `http://localhost:${port}`, // Por el momento solo obtendré el video con id 1
+    baseURL: `http://localhost:3002`, // Por el momento solo obtendré el video con id 1
 });
 
 export default api;
